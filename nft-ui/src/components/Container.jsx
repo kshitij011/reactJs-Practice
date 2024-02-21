d009432442aacf8fb4e04c6838cb1fa953fb9c27
Kshitij Bhoite <kshitijbhoite011@gmail.com>
2024-02-21 20:46:58 +0530

    html and css

---
+++ nft-ui/src/components/Container.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import HappyGuy from "../../public/Images/happy.jpg"
+import CalmGuy from "../../public/Images/calm.png"
+import StraightGuy from "../../public/Images/straight.jpg"
+import ConfusedGuy from "../../public/Images/confused.jpg"
+import AngryGuy from "../../public/Images/angry.jpg"
+import SadGuy from "../../public/Images/sad.jpg"
+
+export const Container = () => {
+  return (
+    <div>
+        <div className='container'>
+            <div className="navigation">
+                <div className="navigation-head">
+                    <i className="uil uil-align-left"></i>
+                </div>
+
+                <div className="grid">
+                    <div className="navigation-menu">
+                        <span className="active flex">
+                            <i className="uil uil-estate"></i>
+                            <small>Home</small>
+                        </span>
+
+                        <span className="flex">
+                            <i className="uil uil-trophy"></i>
+                            <small>Project</small>
+                        </span>
+
+                        <span className="flex">
+                            <i className="uil uil-bill"></i>
+                            <small>Billing</small>
+                        </span>
+
+                        <span className="flex">
+                            <i className="uil uil-graph-bar"></i>
+                            <small>Team Members</small>
+                        </span>
+
+                        <span className="flex">
+                            <i className="uil uil-cog"></i>
+                            <small>Setting</small>
+                        </span>
+                    </div>
+                    <div className="navigation-profile">
+                        <i className="uil uil-user">Computer User</i>
+                    </div>
+                </div>
+            </div>
+
+            <div className="main-container">
+                <div className="box" data='1'>
+                    <img src={HappyGuy} alt="Happy Guy Image" />
+                </div>
+
+                <div className="box" data='2'>
+                    <img src={CalmGuy} alt="Calm Guy Image" />
+                </div>
+
+                <div className="box" data='3'>
+                    <img src={StraightGuy} alt="Straight Guy Image" />
+                </div>
+
+                <div className="box" data='4'>
+                    <img src={ConfusedGuy} alt="Confused Guy Image" />
+                </div>
+
+                <div className="box" data='5'>
+                    <img src={AngryGuy} alt="Angry Guy Image" />
+                </div>
+
+                <div className="box" data='6'>
+                    <img src={SadGuy} alt="Sad Guy Image" />
+                </div>
+            </div>
+        </div>
+
+        <div className="popup">
+            <img src={HappyGuy} alt="Happy Guy" className='change' />
+
+            <div className="text">
+                <h2 className="title">Welcome to NFT Project</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sunt libero molestiae, dolores, iusto saepe illo distinctio minima temporibus a nostrum consequuntur corporis perferendis laudantium soluta, deserunt eligendi vero assumenda.</p>
+
+                <div className="social">
+                    <span>
+                        <i className="uil uil-facebook"></i>
+                    </span>
+                    <span>
+                        <i className="uil uil-instagram"></i>
+                    </span>
+                    <span>
+                        <i className="uil uil-twitter"></i>
+                    </span>
+                    <span>
+                        <i className="uil uil-bitcoin-circle"></i>
+                    </span>
+                </div>
+            </div>
+
+            <div id="btn-close">
+                <i className="uil-multiply"></i>
+            </div>
+        </div>
+
+        <div className="overlay"></div>
+        <div className="lower">
+            <span>
+                <i className="uil uil-atom"></i>
+            </span>
+        </div>
+    </div>
+  )
+}
